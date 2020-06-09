92df71b1e842d473bf5226b83be8fc77d7b02781
Marco Chan <inspiredtolive0@gmail.com>
2020-06-08 23:37:56 -0700

    Creates timer trigger and implements timer

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+/// <summary>
+/// Represents the Timer.
+/// </summary>
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    private float time = 0f;
+
+    /// <summary>
+    /// Updates the timer UI.
+    /// </summary>
+    void Update()
+    {
+        time += Time.deltaTime;
+        timerText.text = string.Format("{0:0}:{1:00}.{2:00}", time / 60, time % 60, time * 100 % 100);
+    }
+}
