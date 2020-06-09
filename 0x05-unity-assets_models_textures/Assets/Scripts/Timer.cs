ec472fa160ef667785e54037bacd7c363d9ebc97
Marco Chan <inspiredtolive0@gmail.com>
2020-06-08 23:46:17 -0700

    Stops the timer when player reaches goal

--- 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -10,13 +10,31 @@ public class Timer : MonoBehaviour
 {
     public Text timerText;
     private float time = 0f;
+    private bool stop = false;
 
     /// <summary>
     /// Updates the timer UI.
     /// </summary>
     void Update()
     {
-        time += Time.deltaTime;
-        timerText.text = string.Format("{0:0}:{1:00}.{2:00}", time / 60, time % 60, time * 100 % 100);
+        if (stop == false)
+        {
+            time += Time.deltaTime;
+            timerText.text = string.Format("{0:0}:{1:00}.{2:00}", time / 60, time % 60, time * 100 % 100);
+        }
+    }
+
+    /// <summary>
+    /// Stops the timer when player reaches the goal.
+    /// </summary>
+    /// <param name="other">The collider that touches the player.</param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "WinFlag")
+        {
+            stop = true;
+            timerText.fontSize = 60;
+            timerText.color = Color.green;
+        }
     }
 }
