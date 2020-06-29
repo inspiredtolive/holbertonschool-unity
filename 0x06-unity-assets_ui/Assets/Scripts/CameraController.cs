cc0cafbf11cbbc2730434600083ce43b217d2ff9
Marco Chan <inspiredtolive0@gmail.com>
2020-06-28 20:38:55 -0700

    Creates asset UI project

---
+++ 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Provides third-person camera tracking.
+/// </summary>
+public class CameraController : MonoBehaviour
+{
+    public Transform playerTransform;
+    private Transform camTransform;
+    float mouseX, mouseY;
+    float distance;
+
+    /// <summary>
+    /// Initializes variables and locks cursor.
+    /// </summary>
+    void Start()
+    {
+        camTransform = GetComponent<Transform>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        distance = Vector3.Distance(camTransform.position, playerTransform.position);
+    }
+
+    /// <summary>
+    /// Updates the camera position based on mouse input.
+    /// </summary>
+    void LateUpdate()
+    {
+        mouseX += Input.GetAxis("Mouse X");
+        mouseY += Input.GetAxis("Mouse Y");
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(-mouseY, mouseX, 0);
+        camTransform.position = playerTransform.position + rotation * dir;
+        
+        transform.LookAt(playerTransform.position);
+    }
+}
---
+++ 0x06-unity-assets_ui/Assets/Scripts/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+/// <summary>
+/// Represents the Timer.
+/// </summary>
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    private float time = 0f;
+    private bool stop = false;
+
+    /// <summary>
+    /// Updates the timer UI.
+    /// </summary>
+    void Update()
+    {
+        if (stop == false)
+        {
+            time += Time.deltaTime;
+            timerText.text = string.Format("{0:0}:{1:00}.{2:00}", time / 60, time % 60, time * 100 % 100);
+        }
+    }
+
+    /// <summary>
+    /// Stops the timer when player reaches the goal.
+    /// </summary>
+    /// <param name="other">The collider that touches the player.</param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "WinFlag")
+        {
+            stop = true;
+            timerText.fontSize = 60;
+            timerText.color = Color.green;
+        }
+    }
+}
