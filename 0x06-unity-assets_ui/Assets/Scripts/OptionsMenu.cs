a2f48e1ac56f642fcc2f6a724a2e37643d27380f
Marco Chan <inspiredtolive0@gmail.com>
2020-06-30 01:06:23 -0700

    Adds level and options scene loading to main menu

---
+++ 0x06-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Represents an options menu.
+/// </summary>
+public class OptionsMenu : MonoBehaviour
+{
+    /// <summary>
+    /// Loads the main menu scene.
+    /// </summary>
+    public void Back()
+    {
+        SceneManager.LoadSceneAsync("MainMenu");
+    }
+}
