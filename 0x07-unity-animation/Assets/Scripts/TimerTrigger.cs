c11ffb66dd1ababad3f5b7326a1a2d6a2c6f7997
Marco Chan <inspiredtolive0@gmail.com>
2020-07-01 19:51:18 -0700

    Duplicates project and creates README

---
+++ 0x07-unity-animation/Assets/Scripts/TimerTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Provides a timer trigger.
+/// </summary>
+public class TimerTrigger : MonoBehaviour
+{
+    public GameObject player;
+
+    /// <summary>
+    /// Triggers the timer when the player moves.
+    /// </summary>
+    /// <param name="other">The collider that exited the trigger area.</param>
+    private void OnTriggerExit(Collider other)
+    {
+        player.GetComponent<Timer>().enabled = true;
+    }
+}
---
+++ 0x07-unity-animation/README.md
@@ -0,0 +1,6 @@
+# Unity - Animation
+
+## Credits
+[Skyboxes: Farland Skies - Cloudy Crown](https://assetstore.unity.com/packages/2d/textures-materials/sky/farland-skies-cloudy-crown-60004#releases)
+
+[Models: Kenney's Nature Pack Extended](https://kenney.nl/assets/nature-pack-extended)
