bf615a03527b2e956ba70dbfa6f90fb384356890
Marco Chan <inspiredtolive0@gmail.com>
2020-11-10 18:52:07 -0800

    Detects horizontal planes

---
+++ 0x0C-unity-ar_slingshot_game/Assets/Scripts/ARSurfaceManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+/// <summary>
+/// Manages plane interactions and UI elements.
+/// </summary>
+public class ARSurfaceManager : MonoBehaviour
+{
+    public ARPlaneManager m_ARPlaneManager;
+    public GameObject m_searchPanel;
+    public GameObject m_instructionPanel;
+    bool m_isPlanesFound = false;
+    
+    void Awake()
+    {
+        m_ARPlaneManager.planesChanged += OnPlanesChanged;
+    }
+
+    void OnPlanesChanged(ARPlanesChangedEventArgs e)
+    {
+        if (!m_isPlanesFound && m_ARPlaneManager.trackables.count > 0)
+        {
+            OnPlanesFound();
+        }
+        else if (m_isPlanesFound && m_ARPlaneManager.trackables.count == 0)
+        {
+            OnPlanesExited();
+        }
+    }
+
+    void OnPlanesFound()
+    {
+        m_isPlanesFound = true;
+        m_searchPanel.SetActive(false);
+        m_instructionPanel.SetActive(true);
+    }
+
+    void OnPlanesExited()
+    {
+        m_isPlanesFound = false;
+        m_searchPanel.SetActive(true);
+        m_instructionPanel.SetActive(false);
+    }
+}
---
+++ 0x0C-unity-ar_slingshot_game/Assets/Scripts/DeselectGameObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// Deselects the current GameObject.
+/// </summary>
+public class DeselectGameObject : MonoBehaviour
+{
+    public void Deselect()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+}
