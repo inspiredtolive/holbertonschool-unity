48386e480a53c17241838985f4adde38a182a2e6
Marco Chan <inspiredtolive0@gmail.com>
2020-11-11 02:17:20 -0800

    Changes to feathered plane material and adds plane selection

--- 0x0C-unity-ar_slingshot_game/Assets/Scripts/ARSurfaceManager.cs
+++ 0x0C-unity-ar_slingshot_game/Assets/Scripts/ARSurfaceManager.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 /// <summary>
 /// Manages plane interactions and UI elements.
 /// </summary>
 public class ARSurfaceManager : MonoBehaviour
 {
+    /// <summary>
+    /// Provides plane detection.
+    /// </summary>
     public ARPlaneManager m_ARPlaneManager;
+    /// <summary>
+    /// Provides plane selection.
+    /// </summary>
+    public ARRaycastManager m_RaycastManager;
+    /// <summary>
+    /// A UI Panel indicating that plane detection is on.
+    /// </summary>
     public GameObject m_searchPanel;
+    /// <summary>
+    /// A UI Panel intructing the user to select a plane.
+    /// </summary>
     public GameObject m_instructionPanel;
+    /// <summary>
+    /// A UI Button to start the game.
+    /// </summary>
+    public GameObject m_startButton;
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
     bool m_isPlanesFound = false;
+    
     
     void Awake()
     {
@@ -43,5 +63,38 @@ public class ARSurfaceManager : MonoBehaviour
         m_isPlanesFound = false;
         m_searchPanel.SetActive(true);
         m_instructionPanel.SetActive(false);
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+
+        touchPosition = default;
+        return false;
+    }
+
+    void Update()
+    {
+        if (m_ARPlaneManager.enabled)
+        {
+            if (!TryGetTouchPosition(out Vector2 touchPosition))
+                return;
+
+            if (m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
+            {
+                m_ARPlaneManager.enabled = false;
+                foreach (ARPlane plane in m_ARPlaneManager.trackables)
+                {
+                    if (plane.trackableId != s_Hits[0].trackableId)
+                        plane.gameObject.SetActive(false);
+                }
+                m_instructionPanel.SetActive(false);
+                m_startButton.SetActive(true);
+            }
+        }
     }
 }
